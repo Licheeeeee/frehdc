09192d4b02780bfc19d7fa610d916e4307dc3d46
EFM user <efmuser@CRWR-EFM-01.(none)>
2019-03-26 11:30:12 -0500

    Remove duplicated directory

--- FrehdC_v41/mpifunctions.h
+++
@@ -1,8 +0,0 @@
-
-
-#include "configuration.h"
-#include "map.h"
-
-void combineAllRanks(double *ally, double *y, Config *setting, int root);
-void mpiexchange(double *S, Maps *map, Config *setting, int irank, int nrank);
-void mpiexchangeInt(int *S, Maps *map, Config *setting, int irank, int nrank);
